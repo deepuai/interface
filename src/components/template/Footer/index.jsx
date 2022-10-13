42f2af957759236d1bfd73f5701a2a8b832e1065
Rafael Mamede <r.silvamamede@gmail.com>
2022-10-13 18:41:29 -0300

    Change in footer

--- src/components/template/Footer/index.jsx
+++ src/components/template/Footer/index.jsx
@@ -8,23 +8,23 @@ const Footer = () => {
                 display="flex"
                 justifyContent="between"
                 alignItems="center"
-                className="mx-auto py-4 flex-wrap"
-                style={{ width: '90%' }}
+                className="mx-auto py-4"
+                style={{ height: '100%' }}
             >
                 <CDBBox display="flex" alignItems="center">
                 <a href="/" className="d-flex align-items-center p-0 text-dark">
                     <img
-                    alt="logo"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/EFEI_logo.png/640px-EFEI_logo.png"
-                    width="30px"
-                    class="me-2"
+                        alt="logo"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/EFEI_logo.png/640px-EFEI_logo.png"
+                        width="40px"
+                        className="ms-2"
                     />
-                    <span className="ml-4 h5 mb-0 font-weight-bold">DeepUAI</span>
+                    <span className="ms-3 h5 mb-0 font-weight-bold">DeepUAI</span>
                 </a>
-                <small className="ml-2">&copy; DeepUAI, 2022. All rights reserved.</small>
+                <small className="ms-5">&copy; DeepUAI, 2022. All rights reserved.</small>
                 </CDBBox>
                 <CDBBox display="flex">
-                <CDBBtn flat color="dark" className="p-2">
+                <CDBBtn href="https://github.com/deeplearnas" flat color="dark" className="p-3 me-4">
                     <CDBIcon fab icon="github" />
                 </CDBBtn>
                 </CDBBox>
